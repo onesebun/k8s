7caf2bacc7c2ac041267e39b5ffb4bc5e1678ace
17 <onesebun@gmail.com>
2019-04-13 23:39:42 +0800

    add sudo

--- install-docker-k8s.sh
+++ install-docker-k8s.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get install curl -y
+sudo apt-get update && apt-get install -y apt-transport-https curl
 
 # install docker
 curl -fsSL https://get.docker.com -o get-docker.sh
@@ -9,14 +9,13 @@ sudo groupadd docker
 sudo usermod -aG docker $USER
 
 # install k8s
-apt-get update && apt-get install -y apt-transport-https curl
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
-apt-get update
-apt-get install -y kubelet kubeadm kubectl
-apt-mark hold kubelet kubeadm kubectl
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
 
 # swapoff
 sudo swapoff -a
